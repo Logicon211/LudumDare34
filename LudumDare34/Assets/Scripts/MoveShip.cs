5f7ccab0db4a7938292ce8b6c442d0ce3e4f741f
KingOfBraves <chadifferus@gmail.com>
2015-12-12 02:03:55 -0400

    Start of ship movement

---
+++ LudumDare34/Assets/Scripts/MoveShip.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveShip : MonoBehaviour {
+
+	public  int currentSpeed = 5; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+			moveLeft();
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+			moveRight();
+	}
+
+
+	void moveLeft() 
+	{
+		Debug.Log("Triggering me left");
+	}
+
+	void moveRight()
+	{
+		Debug.Log("Triggering me right");
+	}
+
+}
