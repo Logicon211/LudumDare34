726d58664036a08b7fd5d8a61d7b105d2ca17add
KingOfBraves <chadifferus@gmail.com>
2015-12-12 12:38:02 -0400

    do ships shit on the open sea if no subs are around?

---
+++ LudumDare34/Assets/Scripts/Player/MoveShip.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Class for controlling the ship
+//Also contains the dual button action
+public class MoveShip : MonoBehaviour {
+
+	public float currentSpeed = 5;
+	Ship ship;
+
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Shiny teeth are go");
+		ship = GetComponent<Ship>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
+			moveLeft();
+		if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
+			moveRight();
+		if ( Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftArrow))
+			useAbility();
+	}
+
+
+	public void moveLeft() 
+	{
+		transform.GetComponent<Rigidbody2D>().velocity = new Vector2(currentSpeed , 0);
+	}
+
+	public void moveRight()
+	{
+		transform.GetComponent<Rigidbody2D>().velocity = new Vector2(-currentSpeed , 0);
+	}
+
+	public void useAbility()
+	{
+		string dbout = "the ships current ability is:" + ship.getAbility();
+		Debug.Log(dbout);
+	}
+
+}
---
+++ LudumDare34/Assets/Scripts/Player/Ship.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Controls the basic stats and shit on this fucking garbage ass ship
+//Here we go
+public class Ship : MonoBehaviour {
+
+	const int DEFAULT_ID = 1;
+	public int currentAbility = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public int getAbility()
+	{
+		return currentAbility;
+	}
+}
