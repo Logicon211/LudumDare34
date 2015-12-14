83775bf7777e5cfff48c6849cd173f9924ff554e
Logicon211 <logicon211@gmail.com>
2015-12-14 14:34:07 -0400

    Added high score

--- LudumDare34/Assets/Scripts/DisplayScore.cs
+++ LudumDare34/Assets/Scripts/DisplayScore.cs
@@ -5,14 +5,28 @@ using UnityEngine.UI;
 public class DisplayScore : MonoBehaviour {
 
 	public Text scoreText;
+	public Text highScoreText;
+	private string highScoreKey = "highScore";
+
+	private int highScore;
 
 	// Use this for initialization
 	void Start () {
 		scoreText.text = ScoreManager.getScore().ToString ();
+
+		highScore = PlayerPrefs.GetInt (highScoreKey, 0);
+
+		if (ScoreManager.getScore() > highScore) {
+			highScore = ScoreManager.getScore ();
+			PlayerPrefs.SetInt(highScoreKey, highScore);
+		}
+
+		highScoreText.text = highScore.ToString ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		scoreText.text = ScoreManager.getScore().ToString ();
+		highScoreText.text = highScore.ToString ();
 	}
 }
