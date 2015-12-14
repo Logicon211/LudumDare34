70f4bc139e2b529139dfafc47bc87ef6dd646a0d
Logicon211 <logicon211@gmail.com>
2015-12-14 13:52:36 -0400

    Adding garbage noise but it won't play

--- LudumDare34/Assets/Scripts/Abilities/Garbage.cs
+++ LudumDare34/Assets/Scripts/Abilities/Garbage.cs
@@ -5,11 +5,23 @@ public class Garbage : PowerUps {
 	
 	//public int abilityId = 1;
 	// Use this for initializatio
-	
-	
+
+	public AudioClip garbageSplat;
+
+	void Update() {
+		//playSound ();
+	}
+
 	override protected void givePowerUp()
 	{
+		playSound ();
 		ship.addGarbage(1);
+	}
+
+	private void playSound() {
+		AudioSource audio = gameObject.GetComponent<AudioSource> ();
+		Debug.Log (audio.clip);
+		audio.Play ();
 	}
 	
 	
